7865bb3ef2dbe2682623008b54ac19f8a0b9af16
Rinat Musin <rinatm@u.yale-nus.edu.sg>
2023-03-26 00:32:20 +0200

    added color format change feature

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 import Slider from 'rc-slider';
 import styles from './Navbar.module.css';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import Snackbar from '@mui/material/Snackbar';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 
 export default class Navbar extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			format: 'hex',
+			open: false
+		};
+		this.handleChange = this.handleChange.bind(this);
+		this.closeSnack = this.closeSnack.bind(this);
+	}
+
+	handleChange(e) {
+		this.setState({ format: e.target.value, open: true }, () => {
+			this.props.changeFormat(this.state.format);
+		});
+	}
+
+	closeSnack() {
+		this.setState({ open: false });
+	}
+
 	render() {
-		const { light, changeLight } = this.props;
+		const { light, changeLight, changeFormat } = this.props;
+		const { format, open } = this.state;
 		return (
 			<header className={styles.Navbar}>
 				<div className={styles.Logo}>
@@ -22,6 +48,28 @@ export default class Navbar extends Component {
 						/>
 					</div>
 				</div>
+				<div className={styles.SelectContainer}>
+					<Select value={format} onChange={this.handleChange}>
+						<MenuItem value="hex">HEX - #ffffff</MenuItem>
+						<MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+						<MenuItem value="rgba">
+							RGBA - rgba(255,255,255,1.0)
+						</MenuItem>
+					</Select>
+				</div>
+				<Snackbar
+					open={open}
+					autoHideDuration={3000}
+					message={
+						<span>Format Changed to {format.toUpperCase()}</span>
+					}
+					action={[
+						<IconButton onClick={this.closeSnack}>
+							<CloseIcon />
+						</IconButton>
+					]}
+					onClose={this.closeSnack}
+				/>
 			</header>
 		);
 	}
--- src/Components/Palette/Palette.js
+++ src/Components/Palette/Palette.js
@@ -7,22 +7,38 @@ export default class Palette extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			light: 500
+			light: 500,
+			format: 'hex'
 		};
 		this.changeLight = this.changeLight.bind(this);
+		this.changeFormat = this.changeFormat.bind(this);
 	}
+
 	changeLight(light) {
 		this.setState({ light });
 	}
+
+	changeFormat(format) {
+		this.setState({ format });
+	}
+
 	render() {
 		const { colors } = this.props.palette;
-		const { light } = this.state;
+		const { light, format } = this.state;
 		const colorBoxes = colors[light].map((color) => (
-			<ColorBox key={color} background={color.hex} name={color.name} />
+			<ColorBox
+				key={color}
+				background={color[format]}
+				name={color.name}
+			/>
 		));
 		return (
 			<div className={styles.Palette}>
-				<Navbar light={light} changeLight={this.changeLight} />
+				<Navbar
+					light={light}
+					changeLight={this.changeLight}
+					changeFormat={this.changeFormat}
+				/>
 				<div className={styles.PaletteColors}>{colorBoxes}</div>
 				{/* Footer here */}
 			</div>
