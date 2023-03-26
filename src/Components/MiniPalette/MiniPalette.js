0ece9c3ae67a1151944908472e8d9839113dc2fc
Rinat Musin <rinatm@u.yale-nus.edu.sg>
2023-03-27 00:32:37 +0300

    beefed up the styling of the index page by adding mini palette components

---
+++ src/Components/MiniPalette/MiniPalette.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { withStyles } from '@material-ui/styles';
+
+const styles = {
+	root: {
+		backgroundColor: 'white',
+		borderRadius: '5px',
+		padding: '0.5rem',
+		position: 'relative',
+		overflow: 'hidden',
+		'&:hover': {
+			cursor: 'pointer'
+		}
+	},
+	colors: {
+		backgroundColor: 'grey',
+		borderRadius: '5px',
+		overflow: 'hidden',
+		width: '100%',
+		display: 'grid',
+		boxSizing: 'border-box',
+		gridTemplateColumns: 'repeat(5,20%)',
+		'& div': {}
+	},
+	boxy: {
+		height: '40px'
+	},
+	title: {
+		display: 'flex',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		margin: '0',
+		color: 'black',
+		paddingTop: '0.5rem',
+		fontSize: '1rem',
+		position: 'relative'
+	},
+	emoji: {
+		marginLeft: '0.5rem',
+		fontSize: '1.5rem'
+	}
+};
+
+function MiniPalette(props) {
+	const { classes, colors, paletteName, emoji } = props;
+	console.log(colors[0].color);
+	return (
+		<div className={classes.root}>
+			<div className={classes.colors}>
+				{colors.map((col) => (
+					<div
+						key={col.name}
+						style={{ backgroundColor: col.color }}
+						className={classes.boxy}
+					/>
+				))}
+			</div>
+			<h5 className={classes.title}>
+				{paletteName}
+				<span className={classes.emoji}>{emoji}</span>
+			</h5>
+		</div>
+	);
+}
+
+export default withStyles(styles)(MiniPalette);
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,16 +4,64 @@ import { generatePalette } from '@/colorHelper';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import styles from './index.module.css';
 import Link from 'next/link';
+import MiniPalette from '@/Components/MiniPalette/MiniPalette';
+import { withStyles } from '@material-ui/styles';
 
-export default function Home() {
+const styles1 = {
+	root: {
+		backgroundColor: 'blue',
+		height: '100vh',
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'flex-start'
+	},
+	container: {
+		width: '50%',
+		display: 'flex',
+		alignItems: 'flex-start',
+		flexDirection: 'column',
+		flexWrap: 'wrap'
+	},
+	nav: {
+		display: 'flex',
+		width: '100%',
+		height: '10vh',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		'& h1': {
+			color: 'white'
+		}
+	},
+	palettes: {
+		width: '100%',
+		boxSizing: 'border-box',
+		display: 'grid',
+		gridTemplateColumns: 'repeat(3,30%)',
+		gridGap: '5%'
+	}
+};
+
+function Home(props) {
+	const { classes } = props;
 	return (
-		<div className={styles.Main}>
-			<h1>Palette List</h1>
-			{seedColors.map((color) => (
-				<Link className={styles.Linky} href={`/palette/${color.id}`}>
-					{color.paletteName}
-				</Link>
-			))}
+		<div className={classes.root}>
+			<div className={classes.container}>
+				<nav className={classes.nav}>
+					<h1>React Colors</h1>
+				</nav>
+				<div className={classes.palettes}>
+					{seedColors.map((color) => (
+						<Link
+							key={color.id}
+							className={styles.Linky}
+							href={`/palette/${color.id}`}
+						>
+							<MiniPalette {...color} />
+						</Link>
+					))}
+				</div>
+			</div>
 		</div>
 	);
 }
+export default withStyles(styles1)(Home);
