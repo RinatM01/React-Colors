83db98b11586838599610b021596c92ac9dcca39
Rinat Musin <rinatm@u.yale-nus.edu.sg>
2023-03-26 16:08:57 +0300

    added basic index page listing all palettes

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,25 +2,18 @@ import Palette from './palette/[palette]';
 import seedColors from './palette/seedColors';
 import { generatePalette } from '@/colorHelper';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import styles from './index.module.css';
+import Link from 'next/link';
 
 export default function Home() {
 	return (
-		// <BrowserRouter>
-		// 	<Switch>
-		// 		<Route
-		// 			exact
-		// 			path="/"
-		// 			render={() => <h1>Pallete List is here</h1>}
-		// 		/>
-		// 		<Route
-		// 			exact
-		// 			path="/palette/:id"
-		// 			render={() => <h1>Individual Palette</h1>}
-		// 		/>
-		// 	</Switch>
-		// </BrowserRouter>
-		<div>
-			<Palette palette={generatePalette(seedColors[5])} />
+		<div className={styles.Main}>
+			<h1>Palette List</h1>
+			{seedColors.map((color) => (
+				<Link className={styles.Linky} href={`/palette/${color.id}`}>
+					{color.paletteName}
+				</Link>
+			))}
 		</div>
 	);
 }
