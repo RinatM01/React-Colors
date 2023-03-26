37b6d4f53937c18d0927f95d0f7d71060c101310
Rinat Musin <rinatm@u.yale-nus.edu.sg>
2023-03-26 15:50:28 +0300

    introduced routing with next js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,24 @@
-import Palette from '../Components/Palette/Palette';
-import seedColors from '../Components/Palette/seedColors';
+import Palette from './palette/[palette]';
+import seedColors from './palette/seedColors';
 import { generatePalette } from '@/colorHelper';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 export default function Home() {
 	return (
+		// <BrowserRouter>
+		// 	<Switch>
+		// 		<Route
+		// 			exact
+		// 			path="/"
+		// 			render={() => <h1>Pallete List is here</h1>}
+		// 		/>
+		// 		<Route
+		// 			exact
+		// 			path="/palette/:id"
+		// 			render={() => <h1>Individual Palette</h1>}
+		// 		/>
+		// 	</Switch>
+		// </BrowserRouter>
 		<div>
 			<Palette palette={generatePalette(seedColors[5])} />
 		</div>
---
+++ src/pages/palette/[palette].js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import ColorBox from '../../Components/ColorBox/ColorBox';
+import styles from './Palette.module.css';
+import Navbar from '../../Components/Navbar/Navbar';
+import seedColors from './seedColors';
+import { useRouter } from 'next/router';
+import { generatePalette } from '@/colorHelper';
+
+export default function Palette() {
+	const [ light, setLight ] = useState(500);
+	const [ format, setFormat ] = useState('hex');
+	const [ palette, setPalette ] = useState();
+	const router = useRouter();
+
+	useEffect(
+		() => {
+			const findPalette = async () => {
+				const paletteId = await router.query.palette;
+				const currPalette = await seedColors.find(function(palette) {
+					return palette.id === paletteId;
+				});
+				if (currPalette) {
+					setPalette(generatePalette(currPalette));
+					console.log(generatePalette(currPalette));
+				}
+			};
+			findPalette();
+		},
+		[ router.query.palette ]
+	);
+
+	const changeLight = (light) => {
+		setLight(light);
+	};
+
+	const changeFormat = (format) => {
+		setFormat(format);
+	};
+
+	return (
+		palette && (
+			<div className={styles.Palette}>
+				<Navbar
+					light={light}
+					changeLight={changeLight}
+					changeFormat={changeFormat}
+				/>
+				<div className={styles.PaletteColors}>
+					{palette.colors[light].map((color) => (
+						<ColorBox
+							key={color.id}
+							background={color[format]}
+							name={color.name}
+						/>
+					))}
+				</div>
+				<footer className={styles.Footer}>
+					{palette.paletteName}
+					<span className={styles.Emoji}>{palette.emoji}</span>
+				</footer>
+			</div>
+		)
+	);
+}
